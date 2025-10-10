1eb9461f071ba56f144f8898c65837e00284474a
uservkai <katemary6623@gmail.com>
2025-10-10 21:23:18 +0300

    fix: added filter capability

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,6 +4,7 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
+    list_filter = ('publication_year', 'author')
     
 # Register your models here.
 admin.site.register(Book, BookAdmin)
