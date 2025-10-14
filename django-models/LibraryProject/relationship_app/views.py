f01b51e670a81ddeb1e9738789522f6b845deab1
uservkai <katemary6623@gmail.com>
2025-10-14 19:58:02 +0300

    views.py 4 order

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Library, Book
 from django.views.generic.detail import DetailView
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
