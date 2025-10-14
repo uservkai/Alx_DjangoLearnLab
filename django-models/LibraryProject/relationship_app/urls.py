f5c14be03eb0dea407de78e6db598c1f423d24d8
uservkai <katemary6623@gmail.com>
2025-10-14 20:40:07 +0300

    In LoginView and LogoutView moved template_name into .as_view()

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('list_books/', views.list_books, name = 'list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name = 'library_detail'),
-    path('login/', LoginView.as_view(), template_name="login"),
-    path('logout/', LogoutView.as_view(), template_name="logout"),
+    path('login/', LoginView.as_view(template_name="login.html"), name="login"),
+    path('logout/', LogoutView.as_view(template_name="logout.html"), name = "logout"),
     path('register/', views.register, name= 'register')
 ]
