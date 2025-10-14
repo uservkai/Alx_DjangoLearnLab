07b6651ecbdfe9cd8053c011f024739a015923d4
uservkai <katemary6623@gmail.com>
2025-10-14 17:39:20 +0300

    fix: regex

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,12 +7,12 @@ def book_list(request):
     """Retrieves all books and renders a template displaying the list"""
     books = Book.objects.all() #fetches all book instances from the db
     context = {'book_list': books} #creates context dic with book list
-    return render (request, 'books/list_books.html', context)
+    return render (request, "relationship_app/list_books.html", context)
     
 class LibraryDetailView(DetailView):
     """ class based view for listing all books available in the db"""
     model = Library
-    template_name = 'relationship_app/library_detail.html'
+    template_name = "relationship_app/library_detail.html"
     context_object_name = 'library'
     
     def get_context_data(self, **kwargs):
