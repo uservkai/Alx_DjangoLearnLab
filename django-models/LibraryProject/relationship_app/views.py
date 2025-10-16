1154bf2264b925c842fa2c5b48fa9a6c6f267895
uservkai <katemary6623@gmail.com>
2025-10-16 19:09:54 +0300

    fix: spelling

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -59,7 +59,7 @@ def admin_view(request):
 
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, 'relationsip_app/librarian_view.html')
+    return render(request, 'relationship_app/librarian_view.html')
 
 @user_passes_test(is_member)
 def member_view(request):
