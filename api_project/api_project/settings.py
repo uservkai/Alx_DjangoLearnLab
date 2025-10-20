9c08c119050feffd28457bb47b0d895adbff1f06
uservkai <katemary6623@gmail.com>
2025-10-20 16:50:52 +0300

    Add initial API app structure with models, views, admin, and tests

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
