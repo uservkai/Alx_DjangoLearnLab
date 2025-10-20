8a531d4b443287af92f2f54b0348d8278546ed27
uservkai <katemary6623@gmail.com>
2025-10-20 20:59:59 +0300

    Add API structure with Book model serializer, views, and URL routing

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
 
 # Create your views here.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
