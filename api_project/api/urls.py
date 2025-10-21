be9d8206a68ce7cc4520d06f77a72df7a071e24f
uservkai <katemary6623@gmail.com>
2025-10-21 11:36:29 +0300

    Add token authentication and permissions to API views and settings

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all') #Registers the BookViewSet with the router
@@ -8,4 +9,5 @@ router.register(r'books_all', BookViewSet, basename='book_all') #Registers the B
 urlpatterns = [
     path('books/', BookList.as_view(), name = 'book_list'), #Maps the book
     path('api/', include(router.urls)), #Includes the router urls for BookViewSet(all CRUD operations)
+    path('api-token-auth/', views.obtain_auth_token, name ='api_token_auth'), #Endpoint to obtain auth token
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -11,3 +15,28 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated] # authentication required
+    
+class BookListView(APIView): 
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated] # authentication required
+    
+    # only authenticated users can view books
+    def get(self, request): 
+        queryset = Book.objects.all()
+        serializer = BookSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class BookCreateView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
+    
+    # only admin users can create books
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) #Created
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) #Bad Request
+    
+            
