5b72bfa11a535b4fd9e6e213ccf5d59b905ec90f
uservkai <katemary6623@gmail.com>
2025-10-22 11:28:03 +0300

    feat: add CRUD operations for Book model with authentication and permissions

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, AuthorListView 
+from .views import BookViewSet, AuthorListView, BookListView, BookDetailView, BookCreateView, BookUpdateView, BookDeleteView 
 from rest_framework.authtoken import views
 
 router = DefaultRouter() #Creates a router for BookViewSet
@@ -9,4 +9,9 @@ router.register(r'books', BookViewSet, basename='book')
 urlpatterns = [
     path('api/books', include(router.urls)), #Includes the router urls for BookViewSet(all CRUD operations)
     path('api/authors/', AuthorListView.as_view(), name='author-list'), #Lists all authors
+    path('api/booklist/', BookListView.as_view(), name = 'book-list'), #Lists all books
+    path('api/books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),#Retrieves details of a specific book by its ID
+    path('api/books/create/', BookCreateView.as_view(), name='book-create'), #Adds a new book
+    path('api/books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'), #Updates an existing book
+    path('api/books/<int:pk>/delete/', BookDeleteView.as_view(), name='bok-delete'), #Removes a book 
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 #Create your views here.
 class AuthorListView(generics.ListAPIView): # lists all authors
@@ -11,3 +14,37 @@ class AuthorListView(generics.ListAPIView): # lists all authors
 class BookViewSet(viewsets.ModelViewSet): # handles all CRUD operations for Book model
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+class BookListView(generics.ListAPIView): #retrieves a list of all books
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly] # allows read-only access for unauthenticated users
+    
+class BookDetailView(generics.RetrieveAPIView): #retrieves details of a specific book by its ID
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly] # allows read-only access for unauthenticated users
+    
+class BookCreateView(generics.CreateAPIView): #adding a new book
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication] # requires token authentication
+    permission_classes = [IsAuthenticated] # only authenticated users can create books
+    
+    def perform_create(self, serializer):
+        serializer.save() # triggers validation and saves the new book instance
+
+class BookUpdateView(generics.UpdateAPIView): #updating and existing book
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication] # requires token authentication
+    permission_classes = [IsAuthenticated] # only authenticated users can update books]
+    
+    def perform_update(self, serializer):
+        serializer.save() # triggers validation and saves the updated book instance
+    
+class BookDeleteView(generics.DestroyAPIView): #removing a book
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication] # requires token authentication
+    permission_classes = [IsAuthenticated] # only authenticated users can delete books
