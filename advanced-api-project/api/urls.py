d75d87d63fdc84e97b2d1fa2000c67ba2f97d8ad
uservkai <katemary6623@gmail.com>
2025-10-22 13:11:07 +0300

    fix: update API paths in urls.py to remove redundant 'api/' prefix

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,11 +7,11 @@ router = DefaultRouter() #Creates a router for BookViewSet
 router.register(r'books', BookViewSet, basename='book') 
 
 urlpatterns = [
-    path('api/books', include(router.urls)), #Includes the router urls for BookViewSet(all CRUD operations)
-    path('api/authors/', AuthorListView.as_view(), name='author-list'), #Lists all authors
-    path('api/booklist/', BookListView.as_view(), name = 'book-list'), #Lists all books
-    path('api/books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),#Retrieves details of a specific book by its ID
-    path('api/books/create/', BookCreateView.as_view(), name='book-create'), #Adds a new book
-    path('api/books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'), #Updates an existing book
-    path('api/books/delete/<int:pk>/', BookDeleteView.as_view(), name='bok-delete'), #Removes a book 
+    path('books', include(router.urls)), #Includes the router urls for BookViewSet(all CRUD operations)
+    path('authors/', AuthorListView.as_view(), name='author-list'), #Lists all authors
+    path('booklist/', BookListView.as_view(), name = 'book-list'), #Lists all books
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),#Retrieves details of a specific book by its ID
+    path('books/create/', BookCreateView.as_view(), name='book-create'), #Adds a new book
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'), #Updates an existing book
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='bok-delete'), #Removes a book 
 ]
