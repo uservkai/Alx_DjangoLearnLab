43233dd399c1622d67e4f33fb4aaa6990670053d
uservkai <katemary6623@gmail.com>
2025-10-23 20:10:06 +0300

    Update URL configurations and enhance profile view with success messages

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,6 +6,12 @@ from . import views
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name = 'login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name = 'logout'),
-    path('register/', views.register, name='register') #register view
-    path('profile/', views.profile_view, name='profile') #profile view
+    path('register/', views.register, name='register'), #register view
+    path('profile/', views.profile_view, name='profile'), #profile view
+    #path search search post via query parameter
+    #path post list lists all blog posts
+    #path post detail shows individual blog post details
+    #path post create creates new blog post
+    #path post update updates existing blog post**
+    #path post delete deletes existing blog post**
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, ProfileForm
-
+from django.contrib import messages # import messages framework
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
 def register(request): # registration of new user
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
@@ -14,12 +14,14 @@ def register(request): # registration of new user
         form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
 
+@login_required #ensure only logged-in users can access
 def profile_view(request):
     profile = request.user.profile
     if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
             form.save()
-        else:
-            form = ProfileForm(instance=profile)
-            return render(request, blog/profile.html, {'form': form})
+            messages.success(request, "Profile updated successfully!")
+    else:
+        form = ProfileForm(instance=profile)
+    return render(request, 'blog/profile.html', {'form': form})
